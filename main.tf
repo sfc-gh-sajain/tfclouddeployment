a5ae1b63e138f6cda2df54f118a9194342e49882
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-11 15:08:37 -0400

    add wh

--- main.tf
+++ main.tf
@@ -64,3 +64,33 @@ resource snowflake_schema PROD_AP_SL_DC_DB_DB__BANKCORE_EXTRACT_DNA {
   is_managed = true
   is_transient = false
 }
+
+resource snowflake_warehouse PROD_DATASCIENCE_WH_WH {
+  name = "PROD_DATASCIENCE_WH_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
+
+resource snowflake_warehouse PROD_ETL_WH_WH {
+  name = "PROD_ETL_WH_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
+
+resource snowflake_warehouse PROD_REPORTING_WH_WH {
+  name = "PROD_REPORTING_WH_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
