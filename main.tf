fe6f42a5e55edda1589fe60cb6e64f08d5b3d3a1
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-11 12:56:49 -0400

    finl commit 1

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source = "Snowflake-Labs/snowflake"
-      version = "0.64.0"
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
     }
   }
 
