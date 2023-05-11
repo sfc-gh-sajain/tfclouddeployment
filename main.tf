a1e3b854128ce7dc72037535ec349697df8f7f30
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-11 12:39:14 -0400

    db commit 3

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "snowflake_schema" "demo_schema" {
   comment  = "Schema for Snowflake Terraform demo"
 }
 
-resource "snowflake_database" "PROD_AP_PL_DC_DB_DB" {
+resource snowflake_database PROD_AP_PL_DC_DB_DB {
   name = "PROD_AP_PL_DC_DB_DB"
 }
 
-resource "snowflake_database" "PROD_AP_RL_DC_DB_DB" {
+resource snowflake_database PROD_AP_RL_DC_DB_DB {
   name = "PROD_AP_RL_DC_DB_DB"
 }
 
-resource "snowflake_database" "PROD_AP_SL_DC_DB_DB" {
+resource snowflake_database PROD_AP_SL_DC_DB_DB {
   name = "PROD_AP_SL_DC_DB_DB"
 }
