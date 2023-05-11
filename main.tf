b982039b5f48cd2a0a6e4d26c4a89504cc6c1525
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-11 11:40:18 -0400

    next COMMIT

--- main.tf
+++ main.tf
@@ -22,3 +22,9 @@ resource "snowflake_database" "demo_new_db" {
   name    = "DEMO_NEW_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_new_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
