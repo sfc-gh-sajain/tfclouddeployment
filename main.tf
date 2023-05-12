cba175aefda8606ea6499f0ac4a6d980dde5c43d
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-12 09:13:42 -0400

    commit 5121

--- main.tf
+++ main.tf
@@ -34,34 +34,3 @@ resource snowflake_database PROD_AP_PL_DC_DB {
   is_transient = false
 }
 
-resource snowflake_schema PROD_AP_PL_DC_DB__BANKCORE_EXTRACT_DNA {
-  database = snowflake_database.PROD_AP_PL_DC_DB.name
-  name = "BANKCORE_EXTRACT_DNA"
-  is_managed = true
-  is_transient = false
-}
-
-resource snowflake_database PROD_AP_RL_DC_DB {
-  name = "PROD_AP_RL_DC_DB"
-  is_transient = false
-}
-
-resource snowflake_schema PROD_AP_RL_DC_DB__BANKCORE_EXTRACT_DNA {
-  database = snowflake_database.PROD_AP_RL_DC_DB.name
-  name = "BANKCORE_EXTRACT_DNA"
-  is_managed = true
-  is_transient = false
-}
-
-resource snowflake_database PROD_AP_SL_DC_DB {
-  name = "PROD_AP_SL_DC_DB"
-  is_transient = false
-}
-
-resource snowflake_schema PROD_AP_SL_DC_DB__BANKCORE_EXTRACT_DNA {
-  database = snowflake_database.PROD_AP_SL_DC_DB.name
-  name = "BANKCORE_EXTRACT_DNA"
-  is_managed = true
-  is_transient = false
-}
-
