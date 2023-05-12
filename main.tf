95866798e715a4ec9d5dfbfe878b441949296315
sfc-gh-samitjain <samit.jain@snowflake.com>
2023-05-12 09:19:33 -0400

    commit 5122

--- main.tf
+++ main.tf
@@ -34,3 +34,64 @@ resource snowflake_database PROD_AP_PL_DC_DB {
   is_transient = false
 }
 
+resource snowflake_schema PROD_AP_PL_DC_DB__BANKCORE_EXTRACT_DNA {
+  database = snowflake_database.PROD_AP_PL_DC_DB.name
+  name = "BANKCORE_EXTRACT_DNA"
+  is_managed = true
+  is_transient = false
+}
+
+resource snowflake_database PROD_AP_RL_DC_DB {
+  name = "PROD_AP_RL_DC_DB"
+  is_transient = false
+}
+
+resource snowflake_schema PROD_AP_RL_DC_DB__BANKCORE_EXTRACT_DNA {
+  database = snowflake_database.PROD_AP_RL_DC_DB.name
+  name = "BANKCORE_EXTRACT_DNA"
+  is_managed = true
+  is_transient = false
+}
+
+resource snowflake_database PROD_AP_SL_DC_DB {
+  name = "PROD_AP_SL_DC_DB"
+  is_transient = false
+}
+
+resource snowflake_schema PROD_AP_SL_DC_DB__BANKCORE_EXTRACT_DNA {
+  database = snowflake_database.PROD_AP_SL_DC_DB.name
+  name = "BANKCORE_EXTRACT_DNA"
+  is_managed = true
+  is_transient = false
+}
+
+resource snowflake_warehouse PROD_DATASCIENCE_WH {
+  name = "PROD_DATASCIENCE_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
+
+resource snowflake_warehouse PROD_ETL_WH {
+  name = "PROD_ETL_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
+
+resource snowflake_warehouse PROD_REPORTING_WH {
+  name = "PROD_REPORTING_WH"
+  warehouse_size = "MEDIUM"
+  initially_suspended = true
+  max_cluster_count = 1
+  min_cluster_count = 1
+  auto_suspend = 600
+  auto_resume = true
+}
+
